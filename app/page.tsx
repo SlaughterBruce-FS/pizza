fff516faa0b8bd039d76cacad99fa56b4bbf5708
SlaughterBruce-FS <BLSlaughter1@student.fullsail.edu>
2024-03-03 18:17:56 -0500

    updated header and page added css reset

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="overflow-hidden">
       <Hero />
       <h1>Home</h1>
     </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,19 +3,20 @@ import React from 'react'
 import bg from '../public/images/hero_bg.jpeg'
 import Image from 'next/image'
 import pizza from '../public/images/pizza.png'
+import Link from 'next/link'
 
 function Hero() {
     return (
-        <div className='w-full h-full px-4  z-0 bg-hero-bg' >
-
-            <div className='grid grid-cols-1  md:grid-cols-2 '>
-                <div className='items-left '>
-                    <h1 className='text-white text-4xl font-bold  pt-20'>Welcome to Yallas Pizza</h1>
-                    <p className='text-white '>The best pizza in town</p>
+        <div className='w-full h-[100vh]  z-0 bg-hero-bg ' >
+            <div className='grid grid-cols-1  md:grid-cols-2 md:items-center '>
+                <div className='items-left py-8 md:py-0 flex flex-col items-center gap-4 '>
+                    <h1 className='text-white text-center text-4xl md:text-6xl font-bold  pt-20 md:pt-0'>Welcome to Yallas Pizza</h1>
+                    <p className='text-white text-center'>The best pizza in town</p>
+                    <Link href="#" className='bg-orange-400 rounded px-4 py-2 text-white font-bold capitalize '>Order now</Link>
                 </div>
 
-                <div className='w-full '>
-                    <Image src={pizza} alt='Pepperoni Pizza' height={800} />
+                <div className='px-20 md:px-0  md:py-20'>
+                    <Image src={pizza} alt='Pepperoni Pizza' className='h-full md:h-100 w-full' />
                 </div>
 
 
