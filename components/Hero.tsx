627f24bdd71c6538c30d2aff3abcfb340f236662
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2024-03-04 21:18:49 -0500

    completed specials and added photos also updated text for why choose us section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ function Hero() {
                 <div className='items-left md:pl-8 py-8 md:py-0 flex flex-col justify-end items-center gap-4 '>
                     <h1 className='text-white text-center text-6xl md:text-9xl font-bold  pt-20 md:pt-0'>Welcome to <span className='text-orange-500'>Yallas </span>Pizza</h1>
                     <p className=' text-center text-2xl md:text-4xl text-orange-500 capitalize tracking-widest '>The best pizza in town</p>
-                    <Link href="#" className='bg-orange-400 rounded px-4 py-2 text-white font-bold capitalize hover:bg-orange-600'>Order now</Link>
+                    <Link href="#" className='bg-orange-400 rounded-full px-4 py-2 text-xl text-white font-bold capitalize hover:bg-orange-600'>Order now</Link>
                 </div>
 
                 <div className='px-20 md:px-0  md:py-20'>
--- components/Specials.tsx
+++ components/Specials.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
 import bg from '../public/images/bottom.png'
 import Image from 'next/image'
+import deal1 from '../public/images/deal1.jpeg'
+import twofour from '../public/images/2four.jpeg'
+import studis from '../public/images/studis.jpeg'
+
+const specials = [
+    {
+        title: 'Family Feast Deal',
+        image: deal1,
+        description: 'Enjoy a large pizza of your choice, along with a choice of garlic bread or cheesy breadsticks, and a 2-liter soda for only $24.99. Perfect for family nights or gatherings with friends.'
+    },
+    {
+        title: 'Two for Tuesday',
+        image: twofour,
+        description: 'Buy any medium or large pizza on Tuesdays and get a second pizza of equal or lesser value for half price! Treat yourself and a friend to a delicious pizza night without breaking the bank'
+    },
+    {
+        title: 'Student Discount',
+        image: studis,
+        description: "Calling all students! Show your student ID and get 15% off your entire order. Whether you're studying late or hanging out with friends, our pizzas are the perfect fuel for your busy student life."
+    }
+]
 
 function Specials() {
     return (
@@ -9,9 +30,22 @@ function Specials() {
                 <Image src={bg} alt='Pizza' height={600} width={2920} />
             </div>
 
-            <div className='py-20'>
+            <div className='py-20 md:py-48 flex flex-col items-center'>
+                <h4 className='text-2xl text-orange-400'>Fresh From Yallas</h4>
+                <h3 className='text-4xl'>OUR SPECIALS</h3>
 
-                special
+                <div className='grid grid-cols-1 mx-auto px-20 mt-8 md:grid-cols-3 md:px-40 gap-8'>
+                    {specials.map((special, index) => (
+                        <div key={index} className='text-center'>
+                            <h5 className='text-2xl font-bold'>{special.title}</h5>
+                            <Image src={special.image} alt='Family eating pizzza' />
+                            <p className='text-md mt-8'>{special.description}</p>
+                        </div>
+                    ))}
+
+                </div>
+
+                <p className='mt-8 italic font-semibold px-20'>** These special offers are valid for a limited time only, so come on down to our restaurant and enjoy some mouthwatering pizza today! **</p>
             </div>
         </>
     )
